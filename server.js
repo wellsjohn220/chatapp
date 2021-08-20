dbc21d3deaded45f76c783fa995073f793e2cc2d
wellsjohn220 <wellsjohn220@gmail.com>
2021-08-20 16:46:08 +1000

    My test commit

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post('/messages', (req, res) => {
         if(err)
             res.sendStatus(500)
         console.log(req.body)
-        //messages.push(req.body);
-        //io.emit('message', req.body)
+        messages.push(req.body);
+        io.emit('message', req.body)
         res.sendStatus(200);
       
     })    
